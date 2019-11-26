d302d46d91a45e250e7097a26154a55f44e5fb3f
Jean78000 <57995197+Jean78000@users.noreply.github.com>
2019-11-26 22:50:05 +0100

    Update map.new.js

--- .github/workflows/map.new.js
+++ .github/workflows/map.new.js
@@ -126,14 +126,75 @@ if (data[i].status !== 'OPEN') {
 MapCycle.init(45.75, 4.85, 10);
 
 // création du canvas
-var canv3 = document.getElementById('cvs47');
-var context3 = canv3.getContext('2d');
-function gereclick(event) { 
-   var XYrect = canv3.getBoundingClientRect();    // action avec le canvas et pas le context
-   var Xcurseur = Math.round(event.clientX - XYrect.left); 
-   var Ycurseur = Math.round(event.clientY - XYrect.top);
-   context3.fillStyle= 'rgb('+Math.floor(Math.random()*255); // = rgb (r)
-   context3.beginPath();
-   context3.arc(Xcurseur, Ycurseur, 1.5,0, 360);
-   context3.fill();
-   }; 
+
+
+var canvasDiv = document.getElementById('canvasDiv');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', 500);
+canvas.setAttribute('height', 300);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+context = canvas.getContext("2d");
+
+$('#canvas').mousedown(function(e){
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+          
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+  });
+
+  $('#canvas').mousemove(function(e){
+    if(paint){
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+      redraw();
+    }
+  });
+
+  $('#canvas').mouseup(function(e){
+    paint = false;
+  });
+
+  $('#canvas').mouseleave(function(e){
+    paint = false;
+  });
+
+  var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+              
+    for(var i=0; i < clickX.length; i++) {		
+      context.beginPath();
+      if(clickDrag[i] && i){
+        context.moveTo(clickX[i-1], clickY[i-1]);
+       }else{
+         context.moveTo(clickX[i]-1, clickY[i]);
+       }
+       context.lineTo(clickX[i], clickY[i]);
+       context.closePath();
+       context.stroke();
+    }
+  }
+
+  canvas = () => {
+    document.getElementById('canvasDiv').style.visibility='visible';
+}
