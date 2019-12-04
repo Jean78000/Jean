4e55404737e6ef6e574f867a3c7987a945884939
Jean78000 <57995197+Jean78000@users.noreply.github.com>
2019-12-04 14:58:19 +0100

    Update slider.POO.js

--- .github/workflows/slider.POO.js
+++ .github/workflows/slider.POO.js
@@ -11,76 +11,89 @@ class Slider {
         this.addKeyListener();
         this.changeImg();
         this.setImages(imagesArray);
-     }
+    }
 
     setImages(images) {
-        if(images.length > 1)
-            {this.images =  images;} 
-        else {alert("Le diaporama est vide");}
+        if (images.length > 1) { this.images = images; }
+        else { alert("Le diaporama est vide"); }
     }
 
-    changeImg() {                                                 
-        if (this.index < this.images.length - 1)                
-            {this.index++;                                       
-            this.src.src = this.images[this.index];                   
-            this.text.textContent = this.legendes[this.index];}        
-        else {this.index = 0;                                      
-            this.src.src = this.images[this.index];                     
-            this.text.textContent = this.legendes[this.index];}               
-            this.timer = setTimeout("this.changeImg()", this.time);             
-            //this.timer = setTimeout(this.changeImg.bind(this), this.time)    
-    }
-
-    suivant() {                                                        
-        if (this.index < this.images.length - 1)                
-            {this.index++;                                         
-            this.src.src = this.images[this.index];                      
-            this.text.textContent = this.legendes[this.index];}                 
-        else {this.index = 0;                                        
-             this.src.src = this.images[this.index];                         
-             this.text.textContent = this.legendes[this.index];}                               
-            clearTimeout(this.timer);
-            this.timer = setTimeout("this.changeImg()", this.time);              
-            clearTimeout(this.timer);                                      
-    }
-
-    precedent() {                                            
-        if (this.index > 0)                                                      
-            {this.index = this.index - 1;                                             
-            this.src.src = this.images[this.index];                                     
-            this.text.textContent = this.legendes[this.index];}                                
-        else {this.index = 3;                                                              
-            this.src.src = this.images[3];                              
-            this.text.textContent = this.legendes[this.index];}                         
-            clearTimeout(this.timer);                                                       
-            this.timer = setTimeout("this.changeImg()", this.time);            
-            clearTimeout(this.timer);                                                
-    }
-
-
-addKeyListener() {
-    $(document).keydown(function(e) {
-        if (e.keyCode === 37) {
-            this.precedent();
-        } else if (e.keyCode === 39) {
-            this.suivant();
+    changeImg() {
+        if (this.index < this.images.length - 1) {
+            this.index++;
+            this.src.src = this.images[this.index];
+            this.text.textContent = this.legendes[this.index];
         }
-    });
-} 
+        else {
+            this.index = 0;
+            this.src.src = this.images[this.index];
+            this.text.textContent = this.legendes[this.index];
+        }
+        this.timer = setTimeout("this.changeImg()", this.time);
+        //this.timer = setTimeout(this.changeImg.bind(this), this.time)    
+    }
 
-//////////////////////////////////////////////
-//////////////////////////////////////////////
+    suivant() {
+        if (this.index < this.images.length - 1) {
+            this.index++;
+            this.src.src = this.images[this.index];
+            this.text.textContent = this.legendes[this.index];
+        }
+        else {
+            this.index = 0;
+            this.src.src = this.images[this.index];
+            this.text.textContent = this.legendes[this.index];
+        }
+        clearTimeout(this.timer);
+        this.timer = setTimeout("this.changeImg()", this.time);
+        clearTimeout(this.timer);
+    }
+
+    precedent() {
+        if (this.index > 0) {
+            this.index = this.index - 1;
+            this.src.src = this.images[this.index];
+            this.text.textContent = this.legendes[this.index];
+        }
+        else {
+            this.index = 3;
+            this.src.src = this.images[3];
+            this.text.textContent = this.legendes[this.index];
+        }
+        clearTimeout(this.timer);
+        this.timer = setTimeout("this.changeImg()", this.time);
+        clearTimeout(this.timer);
+    }
+
+
+    addKeyListener() {
+        $(document).keydown(function (e) {
+            if (e.keyCode === 37) {
+                this.precedent();
+            } else if (e.keyCode === 39) {
+                this.suivant();
+            }
+        });
+    }
+}
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
 
 
 
-window.addEventListener("load", () => {
+window.addEventListener('test', function () {
+
     const images = ["image/image1.PNG",
-              "image/image2.JPG",
-              "image/image3.JPG",
-              "image/image4.JPG"];
+        "image/image2.JPG",
+        "image/image3.JPG",
+        "image/image4.JPG"];
 
-    var legendes = ['Bienvenue sur VELO CLICK','Vous pouvez utiliser les fleches directionelles du clavier', 'Cliquer sur un vélos là oû vous en desirez un','Puis remplissez le formulaire pour signer'];
+    var legendes = ['Bienvenue sur VELO CLICK', 'Vous pouvez utiliser les fleches directionelles du clavier', 'Cliquer sur un vélos là oû vous en desirez un', 'Puis remplissez le formulaire pour signer'];
 
     let slider = new Slider(images, legendes, 5000);
-});
+
+}
+
+
+
 
